7fa1586723cbabf34aaecf6d50a3df85dbf90ba0
Ruan Jansen <ruan.jansen@glucode.com>
2023-08-04 09:01:04 +0200

    Initial commit

---
+++ LeagueOfLegendsCompanion/LeagueOfLegendsCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueOfLegendsCompanionApp.swift
+//  LeagueOfLegendsCompanion
+//
+//  Created by Ruan Jansen on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct LeagueOfLegendsCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
